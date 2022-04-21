4f961417ec3cbb76add9629383c616782c808673
Tal Weiss <Tweiss747@gmail.com>
2022-04-21 14:47:52 +0300

    defined ajv keywords for handling errors in server

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,1 +1,18 @@
-import { JSONSchemaType } from "ajv/dist/ajv";
+import Ajv, { JSONSchemaType } from "ajv/dist/ajv";
+import ajvErrors from 'ajv-errors';
+
+const ajv = new Ajv({allErrors:true})
+ajvErrors(ajv)
+
+
+ajv.addKeyword({
+    keyword:"canBeEmpty",
+    validate: (schema,data) => typeof data === 'string' && data.trim !== ''
+})
+
+ajv.addKeyword({
+    keyword: 'validEmail',
+    validate:(schema, data) => {
+        
+    }
+})
