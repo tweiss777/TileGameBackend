ff01971aae66ece3003a09b8dd916be578bf578c
Tal Weiss <tweiss747@gmail.com>
2022-04-21 17:17:18 +0300

    implemented user schema

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -13,6 +13,50 @@ ajv.addKeyword({
 ajv.addKeyword({
     keyword: 'validEmail',
     validate:(schema, data) => {
-        
+        return typeof data === 'string' && data.match(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
     }
 })
+
+
+
+const userSchema = {
+    type: 'object',
+    properties: {
+        email:{
+            type: 'string',
+            canBeEmpty: false,
+            validEmail: true,
+            errorMessage:{
+                canBeEmpty: 'first name is missing',
+                validEmail: 'email format is invalid'
+            }
+
+        },
+        firstName:{
+            type: 'string',
+            canBeEmpty: false,
+            errorMessage: {
+                canBeEmpty: 'First name is missing'
+            }
+        },
+        lastName: {
+            type: 'string',
+            canBeEmpty: false,
+            errorMessage:{
+                canBeEmpty: 'Last name is missing'
+            }
+        },
+        password:{
+            type: 'string',
+            canBeEmpty: false,
+            errorMessage:{
+                canBeEmpty: "Password is missing"
+            }
+
+        }
+        
+    },
+    required: ['email','firstName','lastName','passWord']
+}
+
+export const validateUser = ajv.compile(userSchema);
