883ccf7383838fcc820943a54865060f887ac629
Dennisoshak <dennisoshak@gmail.com>
2022-04-22 21:34:59 +0300

    Defaukt Port added

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
-import express from 'express'
-import 'dotenv/config';
-import { userRouter } from './routes/signupRoutes.js';
-const PORT = process.env.PORT
+import express from "express";
+import "dotenv/config";
+import { userRouter } from "./routes/signupRoutes.js";
+const PORT = process.env.PORT || 4000;
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.use('/user',userRouter)
+app.use("/user", userRouter);
 
+app.get("/", (req, res) => {
+  res.send("Im alive");
+});
 
-app.listen(PORT, () =>{
-    console.log(`now listening on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`now listening on port ${PORT}`);
+});
