5a7bef72ca98a4c70a5e4a3faf653e119a957126
Tal Weiss <tweiss747@gmail.com>
2022-04-23 13:42:42 +0300

    defined functions for creating user in the database and finding users in the database

---
+++ Services/UserDBFunctions.js
@@ -0,0 +1,22 @@
+
+//pass a user object
+export function createUser(user){
+    //check if user exists
+
+    //encrypt password
+
+
+    // store to database
+
+}
+
+
+
+export function getUser(username,password){
+    //encrypt password to compare with password in database
+
+
+    //return user if exists and passwords match
+}
+
+
