1a3d12ef972b76bae07640049c2461743fcd6758
Dennisoshak <dennisoshak@gmail.com>
2022-04-25 09:48:50 +0300

    added cors

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ import "dotenv/config";
 import { userRouter } from "./routes/signupRoutes.js";
 import { gameRouter } from "./routes/gameRoutes.js";
 import { isSignedOn } from "./middleware/AuthenticationMiddleware.js";
+import cors from "cors";
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/user", userRouter);
 app.use("/game", isSignedOn, gameRouter);
